103e886847e2b7ae6f889fc3a5574fc9bf02cd75
matipoklo <poklojr@gmail.com>
2022-01-26 08:07:43 +0100

    trzeci - poprawki w kodzie/upolszczenie

--- app/src/main/java/com/example/a26012022/MainActivity.kt
+++ app/src/main/java/com/example/a26012022/MainActivity.kt
@@ -13,6 +13,7 @@ class MainActivity : AppCompatActivity() {
 
 
         val CTV = findViewById<CheckedTextView>(R.id.CTV1)
+        val CTV2 = findViewById<CheckedTextView>(R.id.CTV2)
         if (CTV != null) {
             CTV.isChecked = false
             CTV.setCheckMarkDrawable(
@@ -28,14 +29,33 @@ class MainActivity : AppCompatActivity() {
 
                 val msg = getString(R.string.msg_shown)+ " " +
                         getString(if (CTV.isChecked)
-                            R.string.checked else R.string.unchecked)
+                            R.string.zaznaczony else R.string.niezaznaczony)
                 Toast.makeText(this@MainActivity, msg,
                     Toast.LENGTH_SHORT).show()
             }
+            if (CTV2 != null) {
+                CTV2.isChecked = false
+                CTV2.setCheckMarkDrawable(
+                    android.R.drawable.checkbox_off_background)
+
+                CTV2.setOnClickListener {
+                    CTV2.isChecked = !CTV2.isChecked
+                    CTV2.setCheckMarkDrawable(
+                        if (CTV2.isChecked)
+                            android.R.drawable.checkbox_on_background
+                        else
+                            android.R.drawable.checkbox_off_background)
+
+                    val msg = getString(R.string.msg_shown2)+ " " +
+                            getString(if (CTV2.isChecked)
+                                R.string.zaznaczony else R.string.niezaznaczony)
+                    Toast.makeText(this@MainActivity, msg,
+                        Toast.LENGTH_SHORT).show()
+                }
 
 
-    }
+            }
 
 
 }
-}
+}}
